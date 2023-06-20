5025b29ca420f25ca7084f45d711c3692fb2f990
hanguang254 <725204548@qq.com>
2023-06-20 16:16:21 +0800

    renew

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,18 +2,39 @@
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
 //css
 
 //hooks
 import { Web3 } from 'web3';
 import {ethers} from 'ethers';
-import { useNetwork,useAccount } from 'wagmi'
+import { useNetwork,useContractWrite } from 'wagmi'
 import { useEffect,useState } from 'react';
+
+import ABI from '../abi/mint.json';
 
 export default function Home(){
     const { chain } = useNetwork()
     const [Network, setNetwork] = useState(true);
+    const [Loading,setLoading] = useState(true);
 
+    function handleClick() {
+      setLoading(false);
+    }
+
+    const wagmigotchiABI =ABI;
+    // console.log(wagmigotchiABI);
+    const { data, isLoading, isSuccess, write } = useContractWrite({
+        address: '0x92FcBEEddF2625333506837676b20bD5f99Ce9DB',
+        abi: wagmigotchiABI,
+        functionName: '_mint',
+        onSuccess: () => {
+          setLoading(true);
+        },
+        onError: ()=>{
+          setLoading(true);
+        }
+    });
     
     async function switchToZkSync() {
         try {
@@ -57,9 +78,21 @@ export default function Home(){
                 }}>
                     <Box><h1 className='title-drop'>Airdrop is ended!</h1></Box>
                     <Box sx={{color:'white'}}>正文</Box>
-                    <Box>
-                        <Button variant="contained" disabled={!Network}>claim</Button>
-                        <Button variant="contained" disabled={!Network}>mint</Button>
+                    <Box className='button-body' sx={{ display: 'flex',justifyContent:'center' }}>
+                        <Button 
+                            variant="contained" 
+                            disabled={!Network}
+                            color='info'
+                        >claim</Button>
+                        <Box sx={{ width: '50px' }}></Box>
+                        <Button variant="contained" disabled={!Network || !Loading} 
+                            color='info'
+                            onClick={()=>{
+                              handleClick();
+                              write();
+                            }}
+                            
+                          >{Loading?"mint":"Loading.."}</Button>
                     </Box>
                 </Box>
             </Box>
