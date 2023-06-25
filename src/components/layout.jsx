d9f76ab6587564ca37b4401ce39445ce573b75d0
hanguang254 <725204548@qq.com>
2023-06-25 16:50:01 +0800

    renew

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,14 +1,30 @@
-import Navbar from './navbar'
+import React, { useState, useEffect } from "react";
+
+
 
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
-
+import AppNavbar from './App/navbarApp';
+import Navbar from './navbar'
 
 export default function Layout({ children }) {
+
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsSmallScreen(window.innerWidth < 800);
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize(); // 初始化检查一次
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
     return (
       <Container maxWidth='false' disableGutters>
-        <Navbar />
+        {isSmallScreen ? <AppNavbar /> :<Navbar /> }
+        {/* <Navbar /> */}
         <main>{children}</main>
         
       </Container>
